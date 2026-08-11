6d58f711407ae0791eb29c64ff20060dc8e6ef0d
lakshay-aggarwal-codes <08817711624_aiml@vipstc.edu.in>
2026-08-11 18:57:32 +0530

    Implement the menu section

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -32,12 +32,12 @@ const Art = () => {
                 scale: 1.3,
                 maskPosition: 'center',
                 maskSize: '400%',
-                duration: 1,
+                duration: 0.7,
                 ease: 'power1.inOut'
             })
             .to('#masked-content', {
                 opacity: 1,
-                duration: 1,
+                duration: 0.6,
                 ease: 'power1.inOut'
             });
 
